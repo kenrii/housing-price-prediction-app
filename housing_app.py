f0a9991fe2310dcb0669090d8cd3503dded0d149
Ken Riippa <ken.j.riippa@gmail.com>
2021-11-20 18:48:26 +0200

    added prophet predictions and nearest algo model

--- housing_app.py
+++ housing_app.py
@@ -1,12 +1,12 @@
+import json
 import streamlit as st
 import pandas as pd
 import numpy as np
-import joblib
-import pgeocode
+from nearest_postal_code_algo import find_nearest_postal_code
 
 
 st.write(
-"""
+    """
 # 🏠 Housing Price Prediction App
 
 
@@ -17,46 +17,56 @@ Application uses big data based on historic data with machine-learning algorithm
 
 Input your values and test now!
 
-**Disclaimer: At the moment application predicts 2021 Q2 prices for testing purposes.**
+**Disclaimer: At the moment application predicts 2021 Q3 prices.**
 """
 )
 
-nomi = pgeocode.Nominatim("fi")
 
-
-def preprocess_input(postal_code, housing_type):
+def get_predictions(postal_code, housing_type):
     # Encode categorical values to one hot encoding and postal code to geolocational data (latitude, longitude)
-
-    quarter = "2021-01-04"
-    geolocation = nomi.query_postal_code([postal_code])
-    latitude, longitude = (
-        geolocation["latitude"].iloc[0],
-        geolocation["longitude"].iloc[0],
-    )
-    if pd.isnull(latitude) or pd.isnull(longitude):
-        st.error("Application did not find geolocation for this postal code")
+    housing_type_json = get_json_for_housing_type(housing_type)
+    if postal_code in housing_type_json["pred"]:
+        return housing_type_json["pred"][postal_code]
     else:
-        cat = np.array([housing_type, quarter]).reshape(1, -1)
-        load_encoder = joblib.load("onehot_encoder.pkl")
-        features = load_encoder.transform(cat).toarray()
-        features = np.append(features, [longitude, latitude]).reshape(1, -1)
-        return features
+        nearest_postal_code = find_nearest_postal_code(postal_code, housing_type_json)
+        st_disclaimer_nearest_postal_code(True, nearest_postal_code, postal_code)
+        return housing_type_json["pred"][nearest_postal_code]
+
+
+def get_json_for_housing_type(housing_type):
+    json_dict = {
+        "one-room": "one_room_predictions-Prophet.json",
+        "two-room": "two_room_predictions-Prophet.json",
+        "three or more room": "three_room_predictions-Prophet.json",
+        "terrace house": "terraced_houses_predictions-Prophet.json",
+    }
+    json_file_path = "json_prediction/{}".format(json_dict[housing_type])
+
+    with open(json_file_path, "r") as j:
+        contents = json.loads(j.read())
+    return contents
 
 
 def user_input_features():
     # Streamlit interface's inputs to array for predictions
     postal_code = st.text_input("Postal code")
-    housing_type = st.selectbox(
-        "Housing type", ("one-room", "two-room", "three or more room", "terrace house")
-    )
-    return preprocess_input(postal_code, housing_type)
+    housing_type = st.selectbox("Housing type", ("one-room", "two-room", "three or more room", "terrace house"))
+    return postal_code, housing_type
 
 
-try:
-    input_data = user_input_features()
-    load_regr = joblib.load("housing_regr.pkl")
-    pred = load_regr.predict(input_data)
-    st.subheader("Results")
-    st.write("Prediction price (EUR/m2) for 2021 Q2: ***{:d}€***".format(int(pred)))
-except:
-    st.stop()
+def st_disclaimer_nearest_postal_code(found_nearest_postal_code=False, nearest_postal_code=None, postal_code=None):
+    if found_nearest_postal_code:
+        st.write("We didn't find a model for ***{}***. Therefore, we predict with the model which is nearest to this postal code: ***{}***".format(postal_code, nearest_postal_code))
+
+
+if __name__ == "__main__":
+    try:
+        postal_code, housing_type = user_input_features()
+        if postal_code and housing_type:
+            pred = get_predictions(postal_code, housing_type)
+            st.subheader("Results")
+            st.write("Prediction price (EUR/m2) for 2021 Q2:")
+            st.markdown("### {:d}€ ###".format(int(pred)))
+
+    except:
+        st.stop()
---
+++ nearest_postal_code_algo.py
@@ -0,0 +1,24 @@
+import pgeocode
+import numpy as np
+
+
+def find_nearest_postal_code(postal_code, json):
+    nomi = pgeocode.Nominatim("fi")
+    location = nomi.query_postal_code([postal_code])
+    latitude, longitude = location.latitude, location.longitude
+    json_latitude, json_longitude = json["latitude"], json["longitude"]
+
+    nearest_postal_code = "00100"
+    min_distance = np.float("inf")
+    for pc in json_latitude.keys():
+        distance = _min_euclidean_distance(latitude, longitude, json_latitude[pc], json_longitude[pc])
+        if distance < min_distance:
+            min_distance = distance
+            nearest_postal_code = pc
+
+    return nearest_postal_code
+
+
+def _min_euclidean_distance(pc_latitude, pc_longitude, candidate_latitude, candidate_longitude):
+    distance = np.square(candidate_latitude - pc_latitude) + np.square(candidate_longitude - pc_longitude)
+    return distance[0]
